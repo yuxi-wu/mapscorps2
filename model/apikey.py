13d75b10080b230d58ad231aa4d4fb98daa4fe12
Yuxi Wu <yuxiwu@uchicago.edu>
2017-04-14 16:47:49 -0500

    Delete apikey.py

--- model/apikey.py
+++
@@ -1,9 +0,0 @@
-import googlemaps as gm
-
-old = 'AIzaSyCtAQAle9F1AwOOT-NomtBh97cWZTUSGpE'
-
-gmkey = 'AIzaSyBO4JFGqPy6uzXteVe0ZcaBMkbrROtmVAg'
-
-censuskey = 'd18c6f55ad936844ff6e215f5b0ac3fb4bd7cf2d'
-
-gmclient = gm.client.Client(key=gmkey)
