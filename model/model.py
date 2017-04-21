70b3c40979df80782f9d3f3927a439eb74b791bb
Yuxi Wu <yuxiwu@uchicago.edu>
2017-04-20 23:14:32 -0500

    handled different str cases

--- model/model.py
+++ model/model.py
@@ -4,9 +4,7 @@ from .util import calculate_area, get_scores, get_zip_places, get_city_places
 from .apikey import *
 import math
 
-#chicago = pd.read_csv('./model/chicago_training.csv', encoding='utf-8-sig')
 chicago = pd.read_csv('./model/chicago_training.csv', encoding='utf-8-sig')
-
 
 result = sm.ols(formula=\
     'workers_weeks ~ num_places + walkscore + transitscore + bikescore',\
@@ -15,10 +13,11 @@ result = sm.ols(formula=\
 model = dict(result.params)
 
 def predictor_chi(place_type, places, state):
+    place_type = place_type.upper()
     places = places.title().split(',')
     walkscore, transitscore, bikescore = 0,0,0
 
-    if (place_type == 'Neighbourhood') or (place_type == 'ZIP Code'):
+    if (place_type == 'NEIGHBOURHOOD') or (place_type == 'ZIP Code'):
         googlequery = [p + ' ' + state for p in places]
         areas = [calculate_area(gmclient, gq) for gq in googlequery]
         area = sum(areas)
@@ -27,11 +26,11 @@ def predictor_chi(place_type, places, state):
         transitscore = sum([s[1] for s in scores]) / len(places)
         bikescore = sum([s[2] for s in scores]) / len(places)
 
-        if place_type == 'ZIP Code':
+        if place_type == 'ZIP CODE':
             n_p_list = [get_zip_places(p) for p in places]
             num_p = sum(n_p_list)
 
-    elif place_type == 'City':
+    elif place_type == 'CITY':
         googlequery = places[0] + ' ' + state
         area = calculate_area(gmclient, googlequery)
         walkscore, transitscore, bikescore = get_scores(places[0],place_type,state)
