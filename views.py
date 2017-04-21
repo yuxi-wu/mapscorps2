71f4a573f068724461624879c62d7ff9bcb5ee42
Yuxi Wu <yuxiwu@uchicago.edu>
2017-04-20 23:25:51 -0500

    change filenames

--- views.py
+++ views.py
@@ -1,7 +1,6 @@
 from flask import render_template, request
 from app import app
 from form import UrlForm
-from test import tester
 from model import predictor_chi
 
 @app.route("/model")
@@ -9,13 +8,12 @@ from model import predictor_chi
 def homepage():
     print('routed correctly')
     form = UrlForm()
-    return render_template('bias_buster_home.html',
+    return render_template('wp_home.html',
                            title="Worker Predictor",
                            form=form)
 
 @app.route("/results", methods=["post"])
 def results():
-    #print(request.form["url"])
     try:
         data = predictor_chi(request.form['PType'],\
             request.form['Place'], request.form['State'])
@@ -28,6 +26,4 @@ def results():
         print(data)
     except AssertionError:
         data = {'none': ['The news source is not in our database; please enter another article from different news source.']}
-    #data = {'source name': ['center', 'http://thisisafakelink.com', 'title?']}
-    # data = tester(request.form["url"])
-    return render_template("bias_buster_results.html", data=data)
+    return render_template("wp_results.html", data=data)
